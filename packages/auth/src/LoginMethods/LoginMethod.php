283855947aa4267d279e185ffba8bab02f47ff0a
dani <danimohamadnejad@gmail.com>
2022-11-02 20:47:32 +0330

    CU-32pyh8f - Auth

--- packages/auth/src/LoginMethods/LoginMethod.php
+++ packages/auth/src/LoginMethods/LoginMethod.php
@@ -1,29 +1,31 @@
 <?php
 namespace Metft\Auth\LoginMethods;
 use Illuminate\Http\Request;
+use App\Models\User;
 
 abstract class LoginMethod{
  protected ?Request $request = null;
  protected $error = '';
- protected $request_validated = false;
- 
- public function __construct(Request $request){
+ protected ?User $model = null;
+
+ public function __construct(Request $request, User $model){
     $this->request = $request;
+    $this->model = $model;
     $this->error = '';
  }   
-
- protected function set_request_validated($flag){
-   $this->request_validated = $flag;
-   return $this;
+ public function get_request(){
+  return $this->request;
  }
-
+ public function get_model(){
+  return $this->model;
+ }
  protected function set_error($error = ''){
    $this->error = $error;
    return $this;
  }
 
  public abstract function login();
-
+ protected abstract function retrieve_user();
  protected abstract function validate_request() :bool;
 
 }
--- packages/auth/src/LoginMethods/LoginWithIdPass.php
+++ packages/auth/src/LoginMethods/LoginWithIdPass.php
@@ -3,10 +3,26 @@ namespace Metft\Auth\LoginMethods;
 use Metft\Auth\LoginMethods\LoginMethod;
 
 class LoginWithIdPass extends LoginMethod{
+    public const ID_KEYS = ['email',]; 
+    public const PASSWORD_KEY = 'password';
+
     public function login(){    
-          
+     if($this->validate_request()){
+     }
+     return false;
+    }
+    private function get_id_values(){
+      return $this->get_request()->only(static::ID_KEYS);  
     }
     protected function validate_request() : bool{
+      $id_values = $this->get_id_values();
+      foreach($id_values as $k=>$v){
+        if(!empty($v)){
+            return true;
+        }
+      }
+      $this->set_error("Your credentials are wrong.");
+      return false;
     }
 
 }
